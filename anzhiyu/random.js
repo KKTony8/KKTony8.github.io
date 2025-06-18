157f82e589659d6c2b5d8ee2302b4cff39ac2cc7
KKTony8 <a163235304@163.com>
2025-06-18 15:10:41 +0800

    Site updated: 2025-06-18 15:10:40

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/13/About-study/","2025/06/12/My-Weekly-Journal/","2025/06/05/Why/","2025/06/02/my-first-blog/"];function toRandomPost(){
+var posts=["2025/06/13/About-study/","2025/06/12/My-Weekly-Journal/","2025/06/02/my-first-blog/","2025/06/05/Why/"];function toRandomPost(){
     window.location.href='/'+posts[Math.floor(Math.random() * posts.length)];
   };
